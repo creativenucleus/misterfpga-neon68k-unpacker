36eee950db24dee373ea9204ec149bc68d68922e
James <567996+creativenucleus@users.noreply.github.com>
2025-05-12 18:51:21 +0100

    Tweaks and clean

--- README.md
+++ README.md
@@ -1,21 +1,29 @@
 # MisterFPGA Neon68k unpacker
 
-Take a zip file [Neon68k](https://neon68k.com/) and unzip them into an appropriate structure so that you can upload directly to MisterFPGA.
+Take the [Neon68k zip archive](https://neon68k.com/) and unzip one of the collections into an appropriate structure so that it can be upload directly to MisterFPGA.
 
 - Sparsely documented for now, but it should be straightforward to figure out.
-- Only lightly tested. I believe it's not dangerous to use.
+- Only lightly tested. I believe it's not dangerous to use [caveat emptor].
 - I've only tested the Windows version. Relying on Go's cross-platform compile to make good on the other versions!
 
 ## Usage
 
 `.\misterfpga-neon68k-unzipper.exe --src-zip "C:\Users\James\Desktop\neon68k.zip" --collection "Neon68K-20250428\MiSTer Upscaler\English\_Main" --dest-folder .\folder-for-misterfpga`
 
+The contents of the destination folder don't get deleted, so you can run this a few times to bring together a selection of the collections.
+
 ## Possible extensions
 
-- FTP direct to Mister
+- FTP direct to MisterFPGA
 
 ## License and Credit
 
 Credit to James Rutherford and link to the root repository (https://github.com/creativenucleus/misterfpga-neon68k-unzipper)
 
-This is a liberal license - [Code Credit 1.1.0](https://codecreditlicense.com/license/1.1.0) - you are welcome to fork and modify as you like, but please credit me and ideally also send me a mention / pop me a hello if you use it. [Mastodon](https://mastodon.social/@jtruk)
+This is a liberal license - [Code Credit 1.1.0](https://codecreditlicense.com/license/1.1.0) - you are welcome to fork and modify as you like, but ensure you retain this original credit.
+
+Please send me a mention / pop me a hello if you use it :) [Mastodon](https://mastodon.social/@jtruk)
+
+## Thanks
+
+The [Neon68K team](https://neon68k.com/) of course!
--- main.go
+++ main.go
@@ -1,7 +1,5 @@
 package main
 
-// Beware - this is quickly built and could be destructive to file system!
-
 import (
 	"archive/zip"
 	"bytes"
@@ -17,21 +15,21 @@ import (
 func main() {
 	app := &cli.App{
 		Name:  "unzip",
-		Usage: "unzip a load of zip files into a folder",
+		Usage: "unzip a collection of X68000 games into folder ready to upload to MisterFPGA",
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:     "src-zip",
-				Usage:    "src zip file",
+				Usage:    "The source zip file (see info on Neon68K's website for this)",
 				Required: true,
 			},
 			&cli.StringFlag{
 				Name:     "collection",
-				Usage:    "collection folder",
+				Usage:    "The collection folder (which of the subfolders in the zip file to extract)",
 				Required: true,
 			},
 			&cli.StringFlag{
 				Name:     "dest-folder",
-				Usage:    "dest folder",
+				Usage:    "The destination folder for the unzipped files",
 				Required: true,
 			},
 		},
@@ -43,7 +41,7 @@ func main() {
 				log.Fatal("src-zip, collection, and dest-folder are required")
 			}
 
-			err := unzipFromOuterZip(srcZip, collection, destFolder)
+			err := unzipCollectionFromOuterZip(srcZip, collection, destFolder)
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -57,7 +55,7 @@ func main() {
 	}
 }
 
-func unzipFromOuterZip(srcZipFile string, collectionFolder string, destFolder string) error {
+func unzipCollectionFromOuterZip(srcZipFile string, collectionFolder string, destFolder string) error {
 	destFolder, err := filepath.Abs(destFolder)
 	if err != nil {
 		return err
@@ -72,50 +70,53 @@ func unzipFromOuterZip(srcZipFile string, collectionFolder string, destFolder st
 	for _, outerZipFile := range outerZipReader.File {
 		correctedPath := filepath.FromSlash(outerZipFile.Name)
 
-		if strings.HasPrefix(correctedPath, collectionFolder) {
-			log.Println("Unzipping: ", correctedPath)
-
-			outerZipReader, err := outerZipFile.Open()
-			if err != nil {
-				return err
-			}
-			defer outerZipReader.Close()
-
-			buf, err := io.ReadAll(outerZipReader)
-			if err != nil {
-				return err
-			}
-
-			innerZipReader, err := zip.NewReader(bytes.NewReader(buf), int64(len(buf)))
-			if err != nil {
-				return err
-			}
-
-			for _, innerZipFile := range innerZipReader.File {
-				correctedInnerPath := filepath.FromSlash(innerZipFile.Name)
-				correctedInnerPath = filepath.Join(destFolder, correctedInnerPath)
-
-				err := os.MkdirAll(filepath.Dir(correctedInnerPath), os.ModePerm)
-				if err != nil {
-					return err
-				}
-
-				dstFile, err := os.OpenFile(correctedInnerPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModePerm)
-				if err != nil {
-					return err
-				}
-
-				fileInArchive, err := innerZipFile.Open()
-				if err != nil {
-					return err
-				}
-				if _, err := io.Copy(dstFile, fileInArchive); err != nil {
-					return err
-				}
-			}
-
-			log.Println("Successfully unzipped:", correctedPath)
+		// Use only those that match the requested collection path
+		if !strings.HasPrefix(correctedPath, collectionFolder) {
+			continue
 		}
+
+		log.Println("Unzipping: ", correctedPath)
+
+		outerZipFileReader, err := outerZipFile.Open()
+		if err != nil {
+			return err
+		}
+		defer outerZipFileReader.Close()
+
+		outerZipFileBuffer, err := io.ReadAll(outerZipFileReader)
+		if err != nil {
+			return err
+		}
+
+		innerZipReader, err := zip.NewReader(bytes.NewReader(outerZipFileBuffer), int64(len(outerZipFileBuffer)))
+		if err != nil {
+			return err
+		}
+
+		for _, innerZipFile := range innerZipReader.File {
+			correctedInnerPath := filepath.FromSlash(innerZipFile.Name)
+			correctedInnerPath = filepath.Join(destFolder, correctedInnerPath)
+
+			err := os.MkdirAll(filepath.Dir(correctedInnerPath), os.ModePerm)
+			if err != nil {
+				return err
+			}
+
+			destinationFile, err := os.OpenFile(correctedInnerPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModePerm)
+			if err != nil {
+				return err
+			}
+
+			innerZipFileReader, err := innerZipFile.Open()
+			if err != nil {
+				return err
+			}
+			if _, err := io.Copy(destinationFile, innerZipFileReader); err != nil {
+				return err
+			}
+		}
+
+		log.Println("Successfully unzipped:", correctedPath)
 	}
 
 	return nil
